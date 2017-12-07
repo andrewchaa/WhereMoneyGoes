a8d9b0ab50bc0062b80e364e8bd9961b146bbbf8
Andrew Chaa <andrew.chaa@just-eat.com>
2017-12-07 18:12:57 +0000

    first report

--- Report/Program.cs
+++ Report/Program.cs
@@ -4,6 +4,7 @@ using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
+using Newtonsoft.Json;
 
 namespace Report
 {
@@ -11,6 +12,8 @@ namespace Report
     {
         static void Main(string[] args)
         {
+            var uncategorisedExpenses = new List<string>();
+            
             var file = args[0];
 
             var lines = File.ReadAllLines(file).Skip(1);
@@ -22,6 +25,62 @@ namespace Report
                 var expense = Process(records);
                 expenses.Add(expense);
             }
+
+            var categoriesText = File.ReadAllText("Report/Categories.json");
+            var categories = JsonConvert.DeserializeObject<IEnumerable<TokenCategory>>(categoriesText);
+            
+            var reports = new Dictionary<string, decimal>();
+            foreach (var category in categories)
+            {
+                if (reports.ContainsKey(category.Category)) continue;
+                
+                reports.Add(category.Category, 0m);
+            }
+            
+//            reports.Add("Books", 0m);
+//            reports.Add("Charity", 0m);
+//            reports.Add("Communications", 0m);
+//            reports.Add("Dividend", 0m);
+//            reports.Add("Equipment", 0m);
+//            reports.Add("Fixtures", 0m);
+//            reports.Add("Insurance", 0m);
+//            reports.Add("Motor Expenses", 0m);
+//            reports.Add("Office Supplies", 0m);
+//            reports.Add("Postage", 0m);
+//            reports.Add("Public Transport", 0m);
+//            reports.Add("Maintenance", 0m);
+//            reports.Add("Software", 0m);
+//            reports.Add("Subsistence", 0m);
+//            reports.Add("Taxi", 0m);
+//            reports.Add("Tools", 0m);
+//            reports.Add("Travel", 0m);
+            
+            foreach (var expense in expenses)
+            {
+                var category = categories.SingleOrDefault(c => expense.Description.ToLower().Contains(c.Token.ToLower()));
+                if (category != null)
+                {
+                    reports[category.Category] += expense.PaidOut;
+                    
+                }
+                else
+                {
+                    uncategorisedExpenses.Add($"{expense.Description} - {expense.PaidOut}");
+                }
+            }
+
+            Console.WriteLine("Report ...");
+            foreach (var report in reports)
+            {
+                Console.WriteLine($"{report.Key} - {report.Value}");
+            }
+
+            Console.WriteLine("\r\nUncategorised ...");
+            foreach (var uncategorisedExpense in uncategorisedExpenses)
+            {
+                Console.WriteLine(uncategorisedExpense);
+            }
+
         }
 
         private static Expense Process(string[] records)
@@ -35,7 +94,7 @@ namespace Report
                 );
         }
         
-        public static IEnumerable<string> SplitCsv(string input)
+        private static IEnumerable<string> SplitCsv(string input)
         {
             var csvSplit = new Regex("(?:^|,)(\"(?:[^\"]+|\"\")*\"|[^,]*)", RegexOptions.Compiled);
 
@@ -44,6 +103,12 @@ namespace Report
                 yield return match.Value.TrimStart(',');
             }
         }        
+    }
+
+    class TokenCategory
+    {
+        public string Token { get; set; }
+        public string Category { get; set; }
     }
 
     class Expense
@@ -56,9 +121,7 @@ namespace Report
 
         public Expense(string dateTime, string type, string description, string paidOut, string paidIn)
         {
-            Console.WriteLine($"{dateTime}, {type}, {description}, {paidOut}, {paidIn}");
             Date = DateTime.ParseExact(dateTime, "dd MMM yyyy", CultureInfo.InvariantCulture);
-//            Date = DateTime.Parse(dateTime);
             ExpenseType = type;
             Description = description;
             PaidOut = string.IsNullOrWhiteSpace(paidOut) ? 0 : decimal.Parse(paidOut);
