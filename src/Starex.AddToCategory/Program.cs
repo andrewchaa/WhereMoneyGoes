8775b666abac51f7075a88699fc3524310b9008e
Andrew Chaa <andrew.yh.chaa@gmail.com>
2018-07-09 17:31:25 +0100

    add to categories 1

---
+++ src/Starex.AddToCategory/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Amazon.DynamoDBv2;
+
+namespace Starex.AddToCategory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cilent = new AmazonDynamoDBClient();
+            var categories = Table.LoadTable()
+            
+            Console.WriteLine("Hello World!");
+            
+        }
+    }
+}
